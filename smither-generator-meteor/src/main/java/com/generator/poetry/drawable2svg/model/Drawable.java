b94ae91f7861e699deb772f793142b5e389e6667
树朾 <scwang90@hotmail.com>
2017-09-17 22:51:48 +0800

    添加 drawable 到 sbg 的转换项目

---
+++ smither-generator-meteor/src/main/java/com/generator/poetry/drawable2svg/model/Drawable.java
@@ -0,0 +1,51 @@
+package com.generator.poetry.drawable2svg.model;
+
+import com.codesmither.engine.api.IModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 矢量图对象
+ * Created by SCWANG on 2017/9/15.
+ */
+public class Drawable implements IModel {
+
+    private String name;
+    private String viewportWidth;
+    private String viewportHeight;
+
+    private List<Path> paths = new ArrayList<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getViewportWidth() {
+        return viewportWidth;
+    }
+
+    public void setViewportWidth(String viewportWidth) {
+        this.viewportWidth = viewportWidth;
+    }
+
+    public String getViewportHeight() {
+        return viewportHeight;
+    }
+
+    public void setViewportHeight(String viewportHeight) {
+        this.viewportHeight = viewportHeight;
+    }
+
+    public List<Path> getPaths() {
+        return paths;
+    }
+
+    public void setPaths(List<Path> paths) {
+        this.paths = paths;
+    }
+}
