e89a84ec9ba64959b7e73fb40f3f441fea032d07
scwang <scwang90@hotmail.com>
2019-06-14 00:38:12 +0800

    修复数据字典的问题

---
+++ smither-generator-meteor/src/test/java/com/generator/meteor/DataBaseEngineTester.java
@@ -0,0 +1,22 @@
+package com.generator.meteor;
+
+import com.code.smither.project.database.DataBaseConfig;
+import com.code.smither.project.database.DataBaseEngine;
+import com.code.smither.project.database.factory.ConfigFactory;
+
+import org.junit.Test;
+
+public class DataBaseEngineTester {
+
+    @Test
+    public void DataBaseMedicalPlatform() {
+        try {
+            DataBaseConfig config = ConfigFactory.loadConfig("medical-platform.properties");
+            DataBaseEngine engine = new DataBaseEngine(config);
+            engine.launch();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
